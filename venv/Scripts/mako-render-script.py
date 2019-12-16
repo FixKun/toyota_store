8403425005de617069bf8a2764e13569ee285d3c
Ruslan.Zaitsau <bnflfrbvfc115547OMG>
2019-12-16 12:09:17 +0300

    resolved merge conflicts

--- venv/Scripts/mako-render-script.py
+++ venv/Scripts/mako-render-script.py
@@ -1,6 +1,11 @@
 #!D:\Stuff\PycharmProjects\toyota_store\venv\Scripts\python.exe
+<<<<<<< Updated upstream
 # EASY-INSTALL-ENTRY-SCRIPT: 'Mako==1.0.14','console_scripts','mako-render'
 __requires__ = 'Mako==1.0.14'
+=======
+# EASY-INSTALL-ENTRY-SCRIPT: 'Mako==1.1.0','console_scripts','mako-render'
+__requires__ = 'Mako==1.1.0'
+>>>>>>> Stashed changes
 import re
 import sys
 from pkg_resources import load_entry_point
